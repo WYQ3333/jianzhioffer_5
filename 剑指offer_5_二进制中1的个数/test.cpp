7453446f23214aaabb21a2d31f0f0243e80495e0
浅忆梦微凉 <3538922590@qq.com>
2019-01-06 23:03:39 +0800

    测试通过

--- 剑指offer_5_二进制中1的个数/test.cpp
+++ 剑指offer_5_二进制中1的个数/test.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
 
-class Solution {
+class Solution1 {
 public:
 	int  NumberOf1(int n) {
 		int count = 0;
-		int temp = sizeof(int);
+		int temp = sizeof(int)*8;
 		while (temp){
 			if (n & 1 == 1){
 				count++;
@@ -17,10 +17,27 @@ public:
 	}
 };
 
+class Solution2 {
+public:
+	int  NumberOf1(int n) {
+		int count = 0;
+		unsigned int flag = 1;
+		while (flag){
+			if (n & flag){
+				count++;
+			}
+			flag = flag << 1;
+		}
+		return count;
+	}
+};
 int main(){
-	Solution s;
-	cout << s.NumberOf1(-1) << endl;
-
+	cout << "第一种方法" << endl;
+	Solution1 s1;
+	cout << s1.NumberOf1(-1) << endl;
+	cout << "第二种方法" << endl;
+	Solution2 s2;
+	cout << s2.NumberOf1(-1) << endl;
 	system("pause");
 	return 0;
 }
