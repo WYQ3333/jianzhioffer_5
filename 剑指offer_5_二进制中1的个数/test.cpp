592be1cc5ffca0e4356d03afedbea66cde19c72b
浅忆梦微凉 <3538922590@qq.com>
2019-01-06 22:47:26 +0800

    编译通过

---
+++ 剑指offer_5_二进制中1的个数/test.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+	int  NumberOf1(int n) {
+		int count = 0;
+		while (0 != n){
+			if (n ^ 1 == 1){
+				count++;
+			}
+			n >>= 1;
+		}
+		return count;
+	}
+};
+
+int main(){
+	Solution s;
+	cout << s.NumberOf1(2) << endl;
+
+	system("pause");
+	return 0;
+}
